ce20ed9e63d1d4268d183e8ff1b0160ce34298be
JFS <janfschorr@gmail.com>
2018-01-11 11:22:59 -0500

    Added signup actions

--- src/actions/signupActions.js
+++ src/actions/signupActions.js
@@ -1,23 +1,48 @@
-export function saveAccountForm() {
-
+export function saveAccountForm({email, password}) {
+   return {
+       type: 'SAVE_ACCOUNT',
+       payload: {email, password}
+   }
 }
 
-export function saveInitialForm() {
-
+export function submitAccountForm({email, password}) {
+    return {
+        type: 'SUBMIT_ACCOUNT',
+        payload: {email, password}
+    }
 }
 
-export function saveAddressForm() {
-
+export function saveInitialForm({id, first, middle, phone, last, dob}) {
+    return {
+        type: 'SAVE_INITIAL',
+        payload: {id, first, middle, phone, last, dob}
+    }
 }
 
-export function saveIdentityForm() {
-
+export function saveAddressForm({addressLine1, addressLine2, city, province, postalcode, country}) {
+    return {
+        type: 'SAVE_ADDRESS',
+        payload: {addressLine1, addressLine2, city, province, postalcode, country}
+    }
 }
 
-export function submitSignUp() {
+export function saveIdentityForm({sin, idNumber, idtype, idExpiry}) {
+    return {
+        type: 'SAVE_IDENTITY',
+        payload: {sin, idNumber, idtype, idExpiry}
+    }
+}
 
+export function submitSignUp({id, first, middle, phone, last, dob, addressLine1, addressLine2, city, province, postalcode, country, sin, idNumber, idtype, idExpiry}) {
+    return {
+        type: 'SUBMIT_SIGNUP',
+        payload: {id, first, middle, phone, last, dob, addressLine1, addressLine2, city, province, postalcode, country, sin, idNumber, idtype, idExpiry}
+    }
 }
 
 export function submitDocument() {
-    
+    return {
+        type: 'SUBMIT_DOCUMENT',
+        payload: {} // TODO
+    }
 }
